1175ac8b3edd966af93e48e88d541a7f4025a91f
1rhino2 <noreply@github.com>
2025-10-19 02:10:00 -0400

    exec works

--- pkg/commands/shell.go
+++ pkg/commands/shell.go
@@ -1,4 +1,4 @@
-﻿package commands
+package commands
 
 import (
 	"fmt"
@@ -48,3 +48,44 @@ func (c *Commander) ExecuteWithTimeout(command string, timeoutSec int) (string, 
 
 func (c *Commander) StartBackground(command string) (*exec.Cmd, error) {
 	args := append(c.args, command)
+	cmd := exec.Command(c.shell, args...)
+	err := cmd.Start()
+	return cmd, err
+}
+
+func GetSystemInfo() map[string]string {
+	info := make(map[string]string)
+	info["os"] = runtime.GOOS
+	info["arch"] = runtime.GOARCH
+	info["numcpu"] = fmt.Sprintf("%d", runtime.NumCPU())
+
+	if runtime.GOOS == "windows" {
+		if out, err := exec.Command("powershell", "-Command", "Get-ComputerInfo | Select-Object -Property OsName,OsVersion,OsArchitecture | ConvertTo-Json").Output(); err == nil {
+			info["details"] = string(out)
+		}
+	}
+
+	return info
+}
+
+func GetProcessList() (string, error) {
+	var cmd *exec.Cmd
+	if runtime.GOOS == "windows" {
+		cmd = exec.Command("powershell", "-Command", "Get-Process | Select-Object Id,Name,Path,CPU | ConvertTo-Json")
+	} else {
+		cmd = exec.Command("ps", "aux")
+	}
+
+	output, err := cmd.Output()
+	return string(output), err
+}
+
+func KillProcess(pid string) error {
+	var cmd *exec.Cmd
+	if runtime.GOOS == "windows" {
+		cmd = exec.Command("taskkill", "/F", "/PID", pid)
+	} else {
+		cmd = exec.Command("kill", "-9", pid)
+	}
+	return cmd.Run()
+}
