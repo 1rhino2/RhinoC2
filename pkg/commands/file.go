84369b714b178ccabd18ab876384ca170e5da7cf
1rhino2 <noreply@github.com>
2025-10-19 02:32:00 -0400

    upload

--- pkg/commands/file.go
+++ pkg/commands/file.go
@@ -1,1 +1,80 @@
 ﻿package commands
+
+import (
+	"encoding/base64"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+type FileManager struct{}
+
+func NewFileManager() *FileManager {
+	return &FileManager{}
+}
+
+func (fm *FileManager) ReadFile(path string) ([]byte, error) {
+	return os.ReadFile(path)
+}
+
+func (fm *FileManager) ReadFileBase64(path string) (string, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(data), nil
+}
+
+func (fm *FileManager) WriteFile(path string, data []byte) error {
+	dir := filepath.Dir(path)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return err
+	}
+	return os.WriteFile(path, data, 0644)
+}
+
+func (fm *FileManager) WriteFileBase64(path string, encodedData string) error {
+	data, err := base64.StdEncoding.DecodeString(encodedData)
+	if err != nil {
+		return err
+	}
+	return fm.WriteFile(path, data)
+}
+
+func (fm *FileManager) DeleteFile(path string) error {
+	return os.Remove(path)
+}
+
+func (fm *FileManager) ListDirectory(path string) ([]map[string]interface{}, error) {
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var result []map[string]interface{}
+	for _, entry := range entries {
+		info, err := entry.Info()
+		if err != nil {
+			continue
+		}
+
+		item := map[string]interface{}{
+			"name":  entry.Name(),
+			"isDir": entry.IsDir(),
+			"size":  info.Size(),
+			"mode":  info.Mode().String(),
+		}
+		result = append(result, item)
+	}
+	return result, nil
+}
+
+func (fm *FileManager) CreateDirectory(path string) error {
+	return os.MkdirAll(path, 0755)
+}
+
+func (fm *FileManager) CopyFile(src, dst string) error {
+	sourceFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
